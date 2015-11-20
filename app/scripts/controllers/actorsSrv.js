190e64b3a5431fe411ee3b533c4ef84091c59006
infobacon <pablo.g.alarcon@gmail.com>
2015-11-20 15:32:25 -0300

    URL FIX

--- app/scripts/controllers/actorsSrv.js
+++ app/scripts/controllers/actorsSrv.js
@@ -5,3 +5,10 @@ angular.module('angularSpa')
             return $http.get(urlBase);
         };
     });
+angular.module('angularSpa')
+    .service('actorsdetailService', function($http){
+        var urlBase = 'http://localhost:8080/sakila-backend/actors/';
+        this.getActorsId = function(actorId){
+            return $http.get(urlBase+actorId);
+        };
+    });
--- app/scripts/controllers/actorsdetailCtrl.js
+++ app/scripts/controllers/actorsdetailCtrl.js
@@ -1,10 +1,14 @@
-    
     angular.module('angularSpa')
-    .controller('ActorsdetailCtrl', ['$scope','$routeParams','$http',
-        function($scope, $routeParams, $http) {
-            $http.get('http://localhost:8080/sakila-backend/actors/'+ $routeParams.actorId )
-            .success(function(data) {
+    .controller('ActorsdetailCtrl', function($scope, actorsdetailService, $routeParams){
+        $scope.actor =[];
+        function getActorsId(){
+            actorsdetailService.getActorsId($routeParams.actorId)
+            .success(function(data){
                 $scope.actor = data;
+            })
+            .error(function(error){
+                $scope.status = 'Error al consultar por actores';
             });
-            // body...
-        }]);
+        }
+        getActorsId();
+    });
